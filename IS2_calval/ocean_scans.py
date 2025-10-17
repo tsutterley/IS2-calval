7f5ed82408904d874a468d5a9b5e7ba6bc411445
tsutterley <tsutterl@uw.edu>
2025-10-16 20:13:01 -0400

    add granule reads

--- IS2_calval/ocean_scans.py
+++ IS2_calval/ocean_scans.py
@@ -8,7 +8,7 @@ Tech Ref Table:
 https://doi.org/10.5281/zenodo.16283560
 """
 import re
-import timescale
+import h5py
 import numpy as np
 import pandas as pd
 import icesat2_toolkit as is2tk
@@ -16,6 +16,22 @@ import utilities
 
 # get path to data directory
 filepath = utilities.get_data_path(['data'])
+
+# variable mapping
+mapping = {}
+mapping['ATL12'] = dict(
+    delta_time = 'ssh_segments/delta_time',
+    longitude = 'ssh_segments/longitude',
+    latitude = 'ssh_segments/latitude',
+    h = 'ssh_segments/heights/h',
+    h_ice_free = 'ssh_segments/heights/h_ice_free',
+    h_ice_free_uncrtn = 'ssh_segments/heights/h_ice_free_uncrtn',
+    fpb_corr = 'ssh_segments/heights/fpb_corr',
+    ice_conc = 'ssh_segments/stats/ice_conc',
+    n_photon = 'ssh_segments/stats/n_photons',
+    n_ttl_photon = 'ssh_segments/stats/n_ttl_photon',
+    near_sat_fract_seg = 'ssh_segments/stats/near_sat_fract_seg'
+)
 
 def from_excel(xls_file=None, pattern=r'OceanScan'):
     # get excel file if not provided
@@ -67,14 +83,17 @@ def from_excel(xls_file=None, pattern=r'OceanScan'):
     return df
 
 def all_tracks(df):
+    # verify dataframe is iterable
+    if isinstance(df, pd.Series):
+        df = df.to_frame().T
     # build lists with all cycles and RGTs
     cycles, tracks = [], []
     # for each row in the dataframe
     for row in df.itertuples():
         # list of RGTs for row
-        RGT = [r for r in range(row.rgt_start, row.rgt_end+1)]
+        RGT = [r for r in range(int(row.rgt_start), int(row.rgt_end)+1)]
         # cycle as string
-        cycle = [row.cycle]*len(RGT)
+        cycle = [int(row.cycle)]*len(RGT)
         # extend output lists
         cycles.extend(cycle)
         tracks.extend(RGT)
@@ -106,16 +125,77 @@ def cmr(df, product='ATL12', release=7):
     # return the dataframe of granule ids and urls
     return df2
 
-def get_granules(df):
+def get_granules(df, product='ATL12', release=7):
     # get granule ids and urls from CMR
-    df2 = cmr(df, product='ATL12', release=7)
+    df2 = cmr(df, product=product, release=release)
+    # create subdirectory for product and release
+    subdir = filepath.joinpath(f'{product}.{release:07d}')
+    subdir.mkdir(parents=True, exist_ok=True)
     # check that we have each granule
     for i, row in df2.iterrows():
-        granule = filepath.joinpath(row['granule_id'])
+        granule = subdir.joinpath(row['granule_id'])
         if not granule.exists():
             is2tk.utilities.from_nsidc(row['url'], local=granule,
                 timeout=60, verbose=True)
 
+def find_granules(df, product='ATL12', release=7):
+    # get all cycles and RGTs from dataframe
+    df1 = all_tracks(df)
+    # subdirectory for product and release
+    subdir = filepath.joinpath(f'{product}.{release:03d}')
+    # build regular expression for finding granules
+    tracks = df1.track.astype(str).str.zfill(4)
+    cycles = df1.cycle.astype(str).str.zfill(2)
+    orbits = r'|'.join(tracks + cycles)
+    # regular expression pattern for finding granules
+    pattern = rf'{product}_(\d{{14}})_({orbits})\d{{2}}_{release:03d}'
+    rx = re.compile(pattern, re.VERBOSE)
+    granules = [g for g in subdir.iterdir() if rx.match(g.name)]
+    return granules
+
+def read_granule(granule, product='ATL12'):
+    # regular expression pattern for extracting information
+    pattern = r'(ATL\d{2})_(\d{14})_(\d{4})(\d{2})'
+    rx = re.compile(pattern, re.VERBOSE)
+    PRD, YYYYMMDDHHMMSS, RGT, CYC = rx.findall(granule.name).pop()
+    # read data from granule and concatenate into dataframe
+    dataframes = []
+    func = getattr(is2tk.io, product)
+    # read data from each beam
+    with h5py.File(granule, 'r') as fileID:
+        beams = func.find_beams(fileID, KEEP=True)
+        for gtx in beams:
+            # initialize dictionary for storing variables
+            data = {}
+            # extract variables from HDF5 file
+            for key,val in mapping[product].items():
+                data[key] = fileID[gtx][val][:]
+                # apply fill values
+                if hasattr(fileID[gtx][val], 'fillvalue'):
+                    fv = fileID[gtx][val].fillvalue
+                    data[key] = np.ma.masked_equal(data[key], fv)
+            # get derived variables
+            atlas_spot_number = fileID[gtx].attrs['atlas_spot_number']
+            data['atlas_spot_number'] = int(atlas_spot_number)
+            data['ground_track'] = gtx
+            data['track'] = int(RGT)
+            data['cycle'] = int(CYC)
+            # create dataframe and append to list
+            dataframes.append(pd.DataFrame(data))
+    # concatenate dataframes for each beam
+    df = pd.concat(dataframes, ignore_index=True)
+    # return the dataframe
+    return df
+
 if __name__ == '__main__':
+    # read excel file with the tech ref table  
     df = from_excel()
+    # ensure granules are downloaded
     get_granules(df)
+    # for each ocean scan event
+    for i, row in df.iterrows():
+        # find granules for event
+        granules = find_granules(row)
+        # read data from each granule and concatenate into dataframe
+        dataframes = [read_granule(g) for g in granules]
+        df1 = pd.concat(dataframes, ignore_index=True)
